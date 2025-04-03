882e13604faeb72d60f16de283b15b9201e5182a
NaganoMSL228 <9794@holbertonstudents.com>
2025-04-03 13:57:22 +0200

    test for the task 6

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -8,6 +8,7 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([signUpPromise, uploadPhotoPromise])
     .then((results) => results.map((result) => ({
       status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
+      value: result.status === 'fulfilled' ? result.value : undefined,
+      reason: result.status === 'rejected' ? result.reason : undefined,
     })));
 }
