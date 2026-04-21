86be57e99cca838713d42cc7e4122a37114c319a
NaganoMSL228 <9794@holbertonstudents.com>
2026-04-21 11:06:30 +0200

    test for the task one

--- pagination/0-simple_helper_function.py
+++ pagination/0-simple_helper_function.py
@@ -8,9 +8,6 @@ def index_range(page: int, page_size: int) -> tuple:
     Retourne un tuple (start_index, end_index)
     """
 
-    assert isinstance(page, int) and page > 0
-    assert isinstance(page_size, int) and page_size > 0
-
     start_index = (page - 1) * page_size
     end_index = page * page_size
 
