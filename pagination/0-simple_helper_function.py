4b82fdd2b1aad56c26ed60822d6574837b0b3a66
NaganoMSL228 <9794@holbertonstudents.com>
2026-04-21 10:54:27 +0200

    test for the task one

---
+++ pagination/0-simple_helper_function.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""
+Module qui contient la fonction index_range
+"""
+
+def index_range(page: int, page_size: int) -> tuple:
+    """
+    Retourne un tuple (start_index, end_index) correspondant
+    à la plage d'index pour une pagination donnée.
+
+    Args:
+        page (int): numéro de la page (commence à 1)
+        page_size (int): nombre d'éléments par page
+
+    Returns:
+        tuple: (start_index, end_index)
+    """
+
+    start_index = (page - 1) * page_size
+    
+    end_index = page * page_size
+
+    return (start_index, end_index)
