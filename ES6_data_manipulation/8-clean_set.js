c6b45cb1f4e20d0a6105a43788d145d522ae3c20
NaganoMSL228 <9794@holbertonstudents.com>
2026-05-13 10:08:52 +0200

    test for the task 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,10 +1,11 @@
 export default function cleanSet(set, startString) {
-  if (startString === '') {
+  if (!startString) {
     return '';
   }
 
   return Array.from(set)
     .filter((value) => value.startsWith(startString))
     .map((value) => value.slice(startString.length))
+    .filter((value) => value !== '')
     .join('-');
 }
